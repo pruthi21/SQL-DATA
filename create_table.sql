6e4bb599da0c268e55ccdfdcd5d2efb8270eb965
pruthi21 <pruthikaw@gmail.com>
2024-03-15 15:26:28 +0530

    'create_table'

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,6 @@
-create table abc(id int primary key, dob date not null, age int check(age>=18));
+create table abc(id int primary key auto_increment, dob date not null, age int check(age>=18));
 insert into abc values(1,'2024-03-14',18);
 select * from abc;
 select id,date_format(dob,'%W,%D,%M,%Y') as DOB, age from abc;
+select * from abc;
+insert into abc values ('3','2024-04-15',19),(4,'2024-05-14',20),(5,'2024-06-14',19);
