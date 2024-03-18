bc73ae4af8f6e8f160a933f3a1af2e591bac8a1b
pruthi21 <pruthikaw@gmail.com>
2024-03-18 10:54:17 +0530

    'strings'

---
+++ strings(month,upper,dayname).sql
@@ -0,0 +1,7 @@
+create table customer(customerid int primary key,firstname varchar(50),lastname varchar(50), city varchar(40), state varchar(70));
+desc customer;
+select * from customer;
+select dayname('1947-08-15');
+select monthname('2003-12-21');
+select upper('pruthIka');
+select substring('i am doing fullstack python',12);
